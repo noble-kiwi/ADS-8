ceda9e79f4b4333e205e6af63fbe7de28ad5979e
noble_kiwi <iwkr1208@gmail.com>
2026-05-29 16:04:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,8 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <string>
+#include  <vector>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
