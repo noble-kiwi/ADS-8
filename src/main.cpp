a53492c25d79405d5b24f753e1b67b377101b0fa
noble_kiwi <iwkr1208@gmail.com>
2026-05-29 15:06:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,20 @@
 #include "bst.h"
 
 int main() {
+    BST<std::string> tree;
+    const char* filename = "src/war_peace.txt";
+
+    makeTree(tree, filename);
+
+    std::cout << "Tree built.\n"
+              << "Unique words: " << tree.size() << "\n"
+              << "Tree depth: " << tree.depth() << std::endl;
+
+    if (tree.search("war"))
+        std::cout << "Word 'war' found." << std::endl;
+    else
+        std::cout << "Word 'war' not found." << std::endl;
+
+    printFreq(tree);
     return 0;
 }
